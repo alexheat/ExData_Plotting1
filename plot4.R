ed92a9f0305a74df6d5190ec83a3acc6dee1df04
alexheat <aheaton@gmail.com>
2015-06-06 00:24:45 +0000

    Final version
    Matches the chart output

--- plot4.R
+++ plot4.R
@@ -11,10 +11,10 @@ power.df$Date <- strptime(paste(power.df$Date,power.df$Time),format="%d/%m/%Y %H
 power.df <- rename(power.df, DateTime = Date)
 power.df <- select(power.df, -Time)
 
-Render chart and save and PNG
-#png(filename = "plot4.png",
-#    width = 480, height = 480, units = "px", pointsize = 12,
-#    bg = "white")
+#Render chart and save and PNG
+png(filename = "plot4.png",
+    width = 480, height = 480, units = "px", pointsize = 12,
+    bg = "white")
 
 par(mfrow = c(2, 2))
 plot(power.df$DateTime, power.df$Global_active_power, type="l", xlab="", ylab="Global Active Power")
@@ -28,4 +28,4 @@ lines(power.df$DateTime, power.df$Sub_metering_3,  col = "blue")
 legend("topright", c("Sub_metering_1","Sub_metering_2", "Sub_metering_3"),col=c("black", "blue","red"), lty=c(1,1,1),???lwd=1.5, bty="n")
 
 plot(power.df$DateTime, power.df$Global_reactive_power, type="l", xlab="datetime", ylab="Global_reactive_power")
-dev.off()
+dev.off() 
